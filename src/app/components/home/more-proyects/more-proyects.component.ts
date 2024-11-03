74a435d11f22abcfb7240d2bc34442910a3107a0
karan0705 <karan.sharma@akalinfosys.com>
2024-11-03 23:07:51 +0530

    extra content removed

--- src/app/components/home/more-proyects/more-proyects.component.ts
+++ src/app/components/home/more-proyects/more-proyects.component.ts
@@ -14,51 +14,7 @@ projects:any =[];
 
     ngOnInit() {
 
-      this.projects =  [{
-        "Title": "Driver Awareness System",
-        "Description": "Developed a Pyton Code to scan Face of Driver and find out if driver is asleep.Implemented Keras and CNN Methordology to read face postures and determine if driver is asleep as well as if the car is in Motion.",
-        "ghLink": "",
-        "demoLink": "",
-        "Technologies": ["Python", "Machine-Learning", "Keras"]
-    },
-    {
-        "Title": "Handwriting to Text Conversion",
-        "Description": "Developed a Pyton Code to scan Handwriting and convert it into Text. Implemented Keras and CNN Methordology for Conversion of Scanned Material",
-        "ghLink": "",
-        "demoLink": "",
-        "Technologies": ["Python", "Machine-Learning", "Keras"]
-    },
-    {
-        "Title": "Smart Plant",
-        "Description": "Developed a Chipset that can read the Moisture of soil.If Moisture is not at adequate level for a certain time. then supply it with water using motor and a prefilled water storage.",
-        "ghLink": "",
-        "demoLink": "",
-        "Technologies": ["Embedded C", "Aurdino", "Chipset"]
-    },
-    {
-        "Title": "Tic Tac Toe Game",
-        "Description": "Personal project of a game that replicates tic tac toe",
-        "ghLink": "https://mayank200/Tic-tac-toe-game-Angular",
-        "demoLink": "",
-        "Technologies": ["Angular", "CSS", "GIT", "JavaScript"]
-    },
-   
-]
-
-
-        this.router.events.subscribe((evt) => {
-            if (!(evt instanceof NavigationEnd)) {
-                return;
-            }
-            window.scrollTo(0, 0)
-        });
     }
-    redirect(route: string, event:any) {
-      const id = event.target.id;
-      if(id=='demoLink' || id=='ghLink' || route==''){
-        return
-      }
-      window.open(route, '_blank');
-    }
+    
 
 }
